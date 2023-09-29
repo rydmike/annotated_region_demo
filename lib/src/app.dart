bb07747614cd9530df6f46509c57e731025c7216
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-09-29 22:20:26 +0300

    Remove debug banner

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
       listenable: settingsController,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           // Providing a restorationScopeId allows the Navigator built by the
           // MaterialApp to restore the navigation stack when a user leaves and
           // returns to the app after it has been killed while running in the
